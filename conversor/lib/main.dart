9c3f58aa3eb1fbeec423f717df7f0a0046d62a6e
LucasFelipeMota <141691184+LucasFelipeMota@users.noreply.github.com>
2025-11-18 00:43:23 -0300

    refactor: separa função getData e ajusta chamada na main

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request =
@@ -8,10 +8,42 @@ const request =
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  http.Response response = await http.get(Uri.parse(request));
-  print(jsonDecode(response.body));
+
+  //Map<String, dynamic> rates = await getData();
+
+  //print("USD: ${rates['USD']}");
+  //print("BRL: ${rates['BRL']}");
+  //print("EUR: ${rates['EUR']}");
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Container(),
+    home: Home(),
   ));
+}
+
+Future<Map<String, dynamic>> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+  Map<String, dynamic> data = jsonDecode(response.body);
+  return data['data'];
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
